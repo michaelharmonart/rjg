62b4e405a1cd22c33a6ef069a62da06025ab485a
SteveDunn720 <sldunn0501@gmail.com>
2026-01-27 14:34:59 -0700

    Applied Fix to Luciana Correctives

--- build/parts/LucianaCorrectives.py
+++ build/parts/LucianaCorrectives.py
@@ -67,22 +67,24 @@ def Build_Correctives(side='L'):
             rot = mc.getAttr(f"{guide}.jointOrient")[0]
 
 
-            mc.joint(p=pos, name=basejoint_name)
-            mc.xform(basejoint_name, ws=True, ro=rot)
-            mc.makeIdentity(basejoint_name, apply=True, translate=False, rotate=True)
+            mc.select(param)
+            mc.joint(p=pos, name=basejoint_name, o=rot)
+            
+            #mc.xform(basejoint_name, ws=True, ro=rot)
+            #mc.makeIdentity(basejoint_name, apply=True, translate=False, rotate=True)
             mc.setAttr(f'{basejoint_name}.segmentScaleCompensate', 0)
             #mc.parent(basejoint_name, param)
-            rXform.clean_parent(basejoint_name, param)
+            #mc.parent(basejoint_name, param)
+            #rXform.clean_parent(basejoint_name, param)
 
             joint_name = f"{base_name}_{SideShort}_JNT"
             mc.joint(p=pos, name=joint_name)
             mc.xform(joint_name, ws=True, ro=rot)
             mc.makeIdentity(joint_name, apply=True, translate=False, rotate=True)
             mc.setAttr(f'{joint_name}.segmentScaleCompensate', 0)
+            mc.parent(joint_name, basejoint_name)
             #mc.parent(joint_name, basejoint_name)
-            rXform.clean_parent(joint_name, basejoint_name)
-
-
+            #rXform.clean_parent(joint_name, basejoint_name)
             rig_module.tag_bind_joints(joint_name)
     else:
 
